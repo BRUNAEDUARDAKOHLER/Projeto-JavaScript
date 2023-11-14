4831cbdeb3be65737ed448a6d5bd65e4e295708b
BRUNAEDUARDAKOHLER <134429539+BRUNAEDUARDAKOHLER@users.noreply.github.com>
2023-11-14 08:41:24 -0300

    Update

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ let nome = prompt("Qual é o seu nome?");
 alert("Bem-vindo(a),"+ nome +"!");
 
 let mensagem = "Olá, mundo!"; // String
+alert(mensagem);
 
 let diferente = 5 != 3; // Comparação
+alert(diferente);
 
 // If-else
+let soma = prompt("Qual é o seu número");
 if (soma > 10) {
     alert("Soma é maior que 10.");
     } else {
