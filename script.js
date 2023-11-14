de4be16ca01df831435a8ead77aac5fb6d97c7a6
BRUNAEDUARDAKOHLER <134429539+BRUNAEDUARDAKOHLER@users.noreply.github.com>
2023-11-14 08:32:11 -0300

    Update

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ let nome = prompt("Qual é o seu nome?");
 alert("Bem-vindo(a),"+ nome +"!");
 
 let mensagem = "Olá, mundo!"; // String
+
+let diferente = 5 != 3; // Comparação
+
+// If-else
+if (soma > 10) {
+    alert("Soma é maior que 10.");
+    } else {
+    * alert("Soma não é maior que 10.");
+    }
+    n.
+
+    let botaoCor = document.getElementById("botaoMudaCor");
+botaoCor.addEventListener("click", function() {
+document.body.style.backgroundColor = "lightblue";
+});
