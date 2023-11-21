b323604aa2c21f49bd4f5a817992751221b59026
BRUNAEDUARDAKOHLER <134429539+BRUNAEDUARDAKOHLER@users.noreply.github.com>
2023-11-21 08:12:59 -0300

    Update

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let nome = prompt("Qual é o seu nome?");
 alert("Bem-vindo(a),"+ nome +"!");
 
 let mensagem = "Olá, mundo!"; // String
-alert(mensagem);
+alert("Olá, mundo!");
 
 let diferente = 5 != 3; // Comparação
 alert(diferente);
