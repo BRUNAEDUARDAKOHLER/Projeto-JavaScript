5978de91400476d9835c9d9731a801bdb54e81d3
BRUNAEDUARDAKOHLER <134429539+BRUNAEDUARDAKOHLER@users.noreply.github.com>
2023-11-21 08:20:42 -0300

    Update

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ alert("Bem-vindo(a),"+ nome +"!");
 
 let mensagem = "Olá, mundo!"; // String
 alert("Olá, mundo!");
+
+let soma = 5 + 3; // Adição
+
+// If-else
+if (soma &gt; 10) {
+    alert(&quot;Soma é maior que 10.&quot;);
+    } else {
+    
+    * alert(&quot;Soma não é maior que 10.&quot;);
+    }
+    n.
